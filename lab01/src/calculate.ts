3589b6d67a1254c32b658dba23404975cdff5d53
Roman <roma3io@mail.ru>
2025-09-08 20:19:49 +0700

    lab01 done

--- lab01/src/calculate.ts
+++ lab01/src/calculate.ts
@@ -1,20 +1,32 @@
-import { Dict, MatchResult, Semantics } from "ohm-js";
-import grammar, { AddMulActionDict } from "./addmul.ohm-bundle";
+import {Dict, MatchResult, Semantics} from "ohm-js";
+import grammar, {AddMulActionDict} from "./addmul.ohm-bundle";
 
 export const addMulSemantics: AddMulSemantics = grammar.createSemantics() as AddMulSemantics;
 
-
 const addMulCalc = {
-/// write the action rules here
-} satisfies AddMulActionDict<number>
+    number: function(_: any) {
+        return parseInt(this.sourceString);
+    },
+    Exp: function(e: any) {
+        return e.calculate();
+    },
+    AddExp_plus: function(a: any, _: any, b: any) {
+        return a.calculate() + b.calculate();
+    },
+    MulExp_times: function(a: any, _: any, b: any) {
+        return a.calculate() * b.calculate();
+    },
+    PriExp_paren: function(_1: any, e: any, _2: any) {
+        return e.calculate();
+    }
+} satisfies AddMulActionDict<number>;
 
 addMulSemantics.addOperation<Number>("calculate()", addMulCalc);
 
-interface AddMulDict  extends Dict {
+interface AddMulDict extends Dict {
     calculate(): number;
 }
 
-interface AddMulSemantics extends Semantics
-{
+interface AddMulSemantics extends Semantics {
     (match: MatchResult): AddMulDict;
 }
--- lab01/src/index.ts
+++ lab01/src/index.ts
@@ -1,21 +1,22 @@
-import {  MatchResult } from "ohm-js";
-import { addMulSemantics } from "./calculate";
+import {MatchResult} from "ohm-js";
+import {addMulSemantics} from "./calculate";
 import grammar from "./addmul.ohm-bundle";
 
-export function evaluate(content: string): number
-{
+export function evaluate(content: string): number {
     return calculate(parse(content));
 }
-export class SyntaxError extends Error
-{
+
+export class SyntaxError extends Error {
 }
 
-function parse(content: string): MatchResult
-{
-    throw "Not implemented";
+function parse(content: string): MatchResult {
+    const match = grammar.match(content);
+    if (!match.succeeded()) {
+        throw new SyntaxError(match.message || "Syntax error");
+    }
+    return match;
 }
 
-function calculate(expression: MatchResult):number
-{
-    throw "Not implemented"
+function calculate(expression: MatchResult): number {
+    return addMulSemantics(expression).calculate();
 }
