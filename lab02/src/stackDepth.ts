37dbfb1d0b63d7d621c79174665fa8ff509858ee
Roman <roma3io@mail.ru>
2025-09-29 11:15:04 +0700

    better struct

--- lab02/src/stackDepth.ts
+++ lab02/src/stackDepth.ts
@@ -2,28 +2,19 @@ import {ReversePolishNotationActionDict} from "./rpn.ohm-bundle";
 
 export type StackDepth = { max: number, out: number };
 
+const binaryOperator = (left: StackDepth, right: StackDepth): StackDepth => ({
+    max: Math.max(left.max, left.out + right.max),
+    out: left.out + right.out - 1
+});
+
 export const rpnStackDepth = {
     number(a): StackDepth {
         return {max: 1, out: 1};
     },
     Exp_sum(a, b, _): StackDepth {
-        const left = a.stackDepth;
-        const right = b.stackDepth;
-        const maxDepth = Math.max(
-            left.max,
-            left.out + right.max
-        );
-        const outDepth = left.out + right.out - 1;
-        return {max: maxDepth, out: outDepth};
+        return binaryOperator(a.stackDepth, b.stackDepth);
     },
     Exp_mul(a, b, _): StackDepth {
-        const left = a.stackDepth;
-        const right = b.stackDepth;
-        const maxDepth = Math.max(
-            left.max,
-            left.out + right.max
-        );
-        const outDepth = left.out + right.out - 1;
-        return {max: maxDepth, out: outDepth};
+        return binaryOperator(a.stackDepth, b.stackDepth);
     }
 } satisfies ReversePolishNotationActionDict<StackDepth>;
